98304b3255fc7fe6997fd1c4f2cbcc1a156d5cab
kkaylie <kkaylie778@gmail.com>
2025-08-13 21:27:12 -0700

    build: use esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,30 @@
+const { build } = require('esbuild')
+const path = require('path')
+const fs = require('fs')
+
+const certFile = 'us-east-1-bundle.pem'
+const certPath = path.resolve(__dirname, certFile)
+const outDir = 'dist'
+const outCertPath = path.resolve(__dirname, outDir, certFile)
+
+build({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  platform: 'node',
+  target: 'node22',
+  outfile: 'dist/index.js',
+  external: ['pg-native'], // Exclude native dependencies if any, 'pg' sometimes has one
+  sourcemap: true,
+})
+  .then(() => {
+    // esbuild doesn't handle non-JS files, so we copy the cert manually after bundling
+    if (fs.existsSync(certPath)) {
+      fs.copyFileSync(certPath, outCertPath)
+      console.log('Certificate file copied to dist.')
+    }
+    console.log('Build finished successfully.')
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
