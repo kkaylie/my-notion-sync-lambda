b7c71d67ac40a5427a0a0d7def290c2fb5159521
kkaylie <kkaylie778@gmail.com>
2025-08-14 00:22:24 -0700

    refactor: getPool function

--- src/database.ts
+++ src/database.ts
@@ -1,45 +1,7 @@
-import { Pool } from 'pg'
-import path from 'path'
-import fs from 'fs'
 // import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 // import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb'
+import { getPool } from './databaseConfig'
 import type { PostContent } from './types/notion'
-
-const POSTGRESQL_CONFIG = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-}
-const caPath = path.resolve(__dirname, '..', 'us-east-1-bundle.pem')
-
-function getPool() {
-  if (
-    !POSTGRESQL_CONFIG.user ||
-    !POSTGRESQL_CONFIG.host ||
-    !POSTGRESQL_CONFIG.database ||
-    !POSTGRESQL_CONFIG.password
-  ) {
-    throw new Error(
-      'PostgreSQL configuration is incomplete. Please check your environment variables.'
-    )
-  }
-  if (!fs.existsSync(caPath)) {
-    throw new Error(
-      `CA certificate file not found at path: ${caPath}. Please ensure the file exists.`
-    )
-  }
-  const caCertificate = fs.readFileSync(caPath).toString()
-  // --- PostgreSQL config ---
-  const pool = new Pool({
-    ...POSTGRESQL_CONFIG,
-    ssl: {
-      ca: caCertificate,
-    },
-  })
-  return pool
-}
 
 // --- DynamoDB config ---
 // const ddbClient = new DynamoDBClient({ region: process.env.AWS_REGION })
@@ -52,14 +14,7 @@ function getPool() {
  */
 async function upsertPostToPostgres(post: PostContent) {
   const pool = getPool()
-  if (!pool) {
-    throw new Error('Database connection pool is not initialized.')
-  }
-
   const client = await pool.connect()
-  if (!client) {
-    throw new Error('Failed to acquire a database connection.')
-  }
 
   try {
     const existingPost = await client.query(
