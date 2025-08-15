e36f9b4dff5359f87a1d50f220839b650a5dd2cc
kkaylie <kkaylie778@gmail.com>
2025-08-15 11:17:19 -0700

    fix: build in different env

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,19 +4,24 @@ const { build } = require('esbuild')
 const path = require('path')
 const fs = require('fs')
 
+const isProduction = process.env.NODE_ENV === 'production'
+const entryPoints = isProduction
+  ? ['src/index.ts']
+  : ['src/index.ts', 'run-local.ts']
+
 const certFile = `${process.env.AWS_REGION}-bundle.pem`
 const certPath = path.resolve(__dirname, certFile)
 const outDir = 'dist'
 const outCertPath = path.resolve(__dirname, outDir, certFile)
 
 build({
-  entryPoints: ['src/index.ts'],
+  entryPoints,
   bundle: true,
   platform: 'node',
   target: 'node22',
-  outfile: 'dist/index.js',
+  outdir: 'dist',
   external: ['pg-native'], // Exclude native dependencies if any, 'pg' sometimes has one
-  sourcemap: true,
+  sourcemap: !isProduction, // TODO: 'external'
 })
   .then(() => {
     // esbuild doesn't handle non-JS files, so we copy the cert manually after bundling
@@ -24,7 +29,9 @@ build({
       fs.copyFileSync(certPath, outCertPath)
       console.log('Certificate file copied to dist.')
     }
-    console.log('Build finished successfully.')
+    console.log(
+      `Build finished for ${isProduction ? 'production' : 'development'}.`
+    )
   })
   .catch((err) => {
     console.error(err)
